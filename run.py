725241a4060756c175d72c0dd23c2e52b82b639e
Kurmangozhin <47795864+Kurmangozhin@users.noreply.github.com>
2021-12-05 15:50:43 +0600

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-from module import cls
-import argparse
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--i', type = str, help='please image -input')
-    args = parser.parse_args()
-    cls.predictions_classes(args.i)
